b527673b4dca0ff9a4a65b12505986c714ec5eb8
umucyo77 <eumucyuvakumana77@gmail.com>
2026-02-17 15:15:42 +0200

    exercise 2 commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-//Exercise1: array initialization and property Access
+// Exercise 1: array initialization and property Access
 
 const productCategories = ["Books","Electronics","Clothing","Home & Kitchen","Toys & Games"];
 
@@ -20,3 +20,28 @@ productCategories[1]= "Pen";
 
 console.log(productCategories);
 
+// Exercise 2: push , Pop ,shift , unshift Splice
+
+let inventory = [10, 20, 30];
+
+//a 
+
+inventory.push(40);
+console.log(inventory);
+
+//b
+
+inventory.pop();
+console.log(inventory);
+
+//c
+inventory.unshift(0);
+console.log(inventory);
+
+//d 
+inventory.shift();
+console.log(inventory);
+
+//e
+inventory.splice(1,1,25,35);
+console.log(inventory);
